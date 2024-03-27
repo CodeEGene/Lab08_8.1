77561c3e90d99fc0715278bedf1c6bd4521e5d99
ericschmidutz <e-schmidutz@onu.edu>
2024-03-27 17:09:35 -0400

    Completed P8-1.

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int countVowel(string str);
 
 int main() {
-	string word;
-	cin >> word;
-	cout << countVowel(word);
+	string input;
+	while (true) {
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, input);
+		if (input == "Q" || input == "q") break;
+		
+		cout << "Vowel count: " << countVowel(input) << endl;
+
+	}
 	return 0;
 }
 
 int countVowel(string str){
 	int count = 0;
 	for (int i = 0; i < str.length(); i++) {
-		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') { count++; }
+		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') count++;
 	}
 	return count;
 }
